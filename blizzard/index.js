7536d2c333a4cc5919f1cdaa05d1184cd4b16aae
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-03 15:46:43 -0300

    feat: add mobile menu

--- blizzard/index.js
+++ blizzard/index.js
@@ -1,49 +1,51 @@
 const Header = (
     <header>
-      <div className='header container'>
-        <a href="index.html" className="logo">
-            <img src="./assets/logo.svg" alt="Logo da Blizzard" title="Logo da Blizzard" />
-        </a>
-        <nav>
-          <ul>
-            <li>
-              <a href="#">
-                  Jogos
-                  <img src="./assets/arrow-down.svg" alt="Icone de seta" title="Icone de seta" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                  Esportes
-                  <img src="./assets/arrow-down.svg" alt="Icone de seta" title="Icone de seta" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                  Loja
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                  Notícias
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                  Suporte
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className='menu'>
-          <a href="#" className="btn btn-outline">Criar conta</a>
-          <a href="#" className="btn btn-primary">
-              <img src="./assets/icon-perfil.svg" className="" alt="Icone de Login" title="Icone de Login" />
-              Logar
+      <div className='container'>
+        <div className='header'>
+          <a href="index.html" className="logo">
+              <img src="./assets/logo.svg" alt="Logo da Blizzard" title="Logo da Blizzard" />
           </a>
-          <a href="#" className="btn-mobile" id="js-btn-menu-mobile">
-            <img src="./assets/icon-hamburguer.svg" alt="botão menu" title="botão menu" />
-          </a>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">
+                    Jogos
+                    <img src="./assets/arrow-down.svg" alt="Icone de seta" title="Icone de seta" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                    Esportes
+                    <img src="./assets/arrow-down.svg" alt="Icone de seta" title="Icone de seta" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                    Loja
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                    Notícias
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                    Suporte
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className='menu'>
+            <a href="#" className="btn btn-outline">Criar conta</a>
+            <a href="#" className="btn btn-primary">
+                <img src="./assets/icon-perfil.svg" className="" alt="Icone de Login" title="Icone de Login" />
+                Logar
+            </a>
+            <a href="#" className="btn-mobile" id="js-btn-menu-mobile">
+              <img src="./assets/icon-hamburguer.svg" alt="botão menu" title="botão menu" />
+            </a>
+          </div>
         </div>
       </div>
     </header>
